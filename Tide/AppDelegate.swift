544e00c8c0f8b5d39ed4d42b2aeea2ab03dcefbb
Michael Hoy <mjh@mjhoy.com>
2016-11-15 15:39:42 -0600

    use xcode

--- Tide/AppDelegate.swift
+++ Tide/AppDelegate.swift
@@ -1,69 +1,80 @@
-import AppKit
+//
+//  AppDelegate.swift
+//  Tide
+//
+//  Created by Michael Hoy on 11/15/16.
+//  Copyright © 2016 mjhoy. All rights reserved.
+//
 
-class AppController : NSObject, NSApplicationDelegate {
+import Cocoa
 
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
     var statusItem: NSStatusItem?
     var timer: Timer?
     var targetDate: Date?
 
-    func applicationDidFinishLaunching(_ n: Notification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         let s = NSStatusBar.system()
         let item = s.statusItem(withLength: NSVariableStatusItemLength)
         self.statusItem = item
+        
         if let button = item.button {
-            button.title = "Tide"
+                button.title = "  "
         }
+        
         let menu = NSMenu()
-
-        menu.addItem(NSMenuItem(title: "25 minutes", action: #selector(start(_:)), keyEquivalent: ""))
-        menu.addItem(NSMenuItem(title: "5 minutes", action: #selector(start(_:)), keyEquivalent: ""))
+        
+        menu.addItem(NSMenuItem(title: "⏲ 25", action: #selector(start(_:)), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "⏲ 5", action: #selector(start(_:)), keyEquivalent: ""))
         menu.addItem(NSMenuItem(title: "Cancel", action: #selector(cancel), keyEquivalent: ""))
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
-
+        
         item.menu = menu
         update()
     }
-
+    
     func cancel(_ item: NSMenuItem?) {
         timer?.invalidate()
         timer = nil
         targetDate = nil
         update()
     }
-
+    
     func quit() {
         let app = NSApp
         app?.terminate(self)
     }
-
+    
     func start(_ item: NSMenuItem?) {
         timer?.invalidate()
         timer = nil
-
+        
         if let title = item?.title {
             let t: Int = {
                 switch (title) {
-                case "25 minutes":
+                case "⏲ 25":
                     return 25
-                case "5 minutes":
+                case "⏲ 5":
                     return 5
                 default:
                     NSLog("Unknown menu item")
                     return 5
                 }
             }()
-
+            
             targetDate = Date.init(timeIntervalSinceNow: (Double(t * 60)))
-
+            
             timer = Timer.scheduledTimer(timeInterval: 1, // fires every second
-                                         target: self,
-                                         selector: #selector(update),
-                                         userInfo: nil,
-                                         repeats: true)
+                target: self,
+                selector: #selector(update),
+                userInfo: nil,
+                repeats: true)
             update()
         }
     }
-
+    
     func update() {
         let now = Date.init()
         if let d = targetDate?.timeIntervalSince(now) {
@@ -71,42 +82,39 @@ class AppController : NSObject, NSApplicationDelegate {
             let m  = di / 60
             let s  = di % 60
             let ss: String = { if (s < 10) { return "0\(s)" } else { return "\(s)" } }()
-
+            
             if (d > 0) {
                 if let button = statusItem?.button {
                     button.title = "\(m):\(ss)"
                 }
             } else {
-
+                
                 timer?.invalidate()
                 timer = nil
                 if let button = statusItem?.button {
-                    button.title = "Tide"
+                    button.title = "🌊"
                 }
-
+                
                 utter("Hello, your time is up")
             }
         } else {
             timer?.invalidate()
             timer = nil
             if let button = statusItem?.button {
-                button.title = "Tide"
+                button.title = "🌊"
             }
         }
     }
-
+    
     func utter(_ t: String) {
         let synthesizer = NSSpeechSynthesizer.init(voice: "com.apple.speech.synthesis.voice.Alex")
         synthesizer?.startSpeaking(t)
     }
 
-    func applicationWillTerminate(_ n: Notification) {
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
     }
+
+
 }
 
-NSApplication.shared()
-
-let controller = AppController()
-NSApp.delegate = controller
-
-NSApp.run()
